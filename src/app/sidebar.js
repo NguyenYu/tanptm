ba28ad7129eea10a58e0d3fd69d0687742bec848
nguyenvu <vu12091999@gmail.com>
2026-06-10 22:02:51 +0700

    hello

--- src/app/sidebar.js
+++ src/app/sidebar.js
@@ -1,35 +1,50 @@
-import Link from "next/link"
-
+import Link from 'next/link';
 
 const SideBar = () => {
     return (
-        <div className="w-full h-full">
-            <div className="w-full h-11 bg-primary text-primary-text flex items-center justify-center rounded-r-3xl">
-                <Link href="/report" className="w-full h-full bg-primary text-primary-text hover:bg-hover transition-all duration-300 hover:rounded-r-3xl flex items-center justify-center">
+        <div className="h-full w-full">
+            <div className="bg-primary text-primary-text flex h-11 w-full items-center justify-center rounded-r-3xl">
+                <Link
+                    href="/"
+                    className="bg-primary text-primary-text hover:bg-hover flex h-full w-full items-center justify-center transition-all duration-300 hover:rounded-r-3xl"
+                >
                     <p className="text-size-primary font-medium">Biển bản</p>
                 </Link>
             </div>
-            <div className="w-full h-11 bg-primary text-primary-text flex items-center justify-center rounded-r-3xl">
-                <Link href="/report" className="w-full h-full bg-primary text-primary-text hover:bg-hover transition-all duration-300 hover:rounded-r-3xl flex items-center justify-center">
-                    <p className="text-size-primary font-medium">Biển bản</p>
-                </Link>
-            </div><div className="w-full h-11 bg-primary text-primary-text flex items-center justify-center rounded-r-3xl">
-                <Link href="/report" className="w-full h-full bg-primary text-primary-text hover:bg-hover transition-all duration-300 hover:rounded-r-3xl flex items-center justify-center">
-                    <p className="text-size-primary font-medium">Biển bản</p>
-                </Link>
-            </div>
-            <div className="w-full h-11 bg-primary text-primary-text flex items-center justify-center rounded-r-3xl">
-                <Link href="/report" className="w-full h-full bg-primary text-primary-text hover:bg-hover transition-all duration-300 hover:rounded-r-3xl flex items-center justify-center">
+            <div className="bg-primary text-primary-text flex h-11 w-full items-center justify-center rounded-r-3xl">
+                <Link
+                    href="/"
+                    className="bg-primary text-primary-text hover:bg-hover flex h-full w-full items-center justify-center transition-all duration-300 hover:rounded-r-3xl"
+                >
                     <p className="text-size-primary font-medium">Biển bản</p>
                 </Link>
             </div>
-            <div className="w-full h-11 bg-primary text-primary-text flex items-center justify-center rounded-r-3xl">
-                <Link href="/report" className="w-full h-full bg-primary text-primary-text hover:bg-hover transition-all duration-300 hover:rounded-r-3xl flex items-center justify-center">
+            <div className="bg-primary text-primary-text flex h-11 w-full items-center justify-center rounded-r-3xl">
+                <Link
+                    href="/"
+                    className="bg-primary text-primary-text hover:bg-hover flex h-full w-full items-center justify-center transition-all duration-300 hover:rounded-r-3xl"
+                >
+                    <p className="text-size-primary font-medium">Biển bản</p>
+                </Link>
+            </div>
+            <div className="bg-primary text-primary-text flex h-11 w-full items-center justify-center rounded-r-3xl">
+                <Link
+                    href="/"
+                    className="bg-primary text-primary-text hover:bg-hover flex h-full w-full items-center justify-center transition-all duration-300 hover:rounded-r-3xl"
+                >
+                    <p className="text-size-primary font-medium">Biển bản</p>
+                </Link>
+            </div>
+            <div className="bg-primary text-primary-text flex h-11 w-full items-center justify-center rounded-r-3xl">
+                <Link
+                    href="/"
+                    className="bg-primary text-primary-text hover:bg-hover flex h-full w-full items-center justify-center transition-all duration-300 hover:rounded-r-3xl"
+                >
                     <p className="text-size-primary font-medium">Biển bản</p>
                 </Link>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default SideBar
+export default SideBar;
